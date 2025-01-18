2eb01fc24707034d49b94e464fc845ee8e83ca57
postassiums <batataprogramadora@gmail.com>
2025-01-18 20:17:17 -0300

    :sparkles: feat: Using ChatBubbles

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,11 +1,13 @@
 <template>
   <q-page padding >
-    <SpeechContainer @speech_end="setupTranscription" >
+    <SpeechContainer @speech_end="onSpeechStop" >
 
     </SpeechContainer>
-    <TranscriptionInput v-if="base64_data!=undefined" :data="base64_data" >
 
-    </TranscriptionInput>
+
+    <ChatBubbles v-if="conversations.length>0" :conversations="conversations">
+
+    </ChatBubbles>
 
   </q-page>
 
@@ -18,16 +20,59 @@ import TranscriptionInput from 'src/components/TranscriptionInput.vue';
 import SpeechContainer from 'src/components/SpeechContainer.vue';
 import { chatgpt } from 'src/service';
 import { ref } from 'vue';
+import { Conversation } from 'src/components/models';
+import ChatBubbles from 'src/components/ChatBubbles.vue';
 
-let base64_data=ref()
 
-async function setupTranscription(audio : Float32Array<ArrayBufferLike>,base64 : string )
+let conversations=ref<Conversation[]>([])
+
+
+async function onSpeechStop(audio : Float32Array<ArrayBufferLike>,base64 : string )
 {
-  base64_data.value=base64
+  try{
+    conversations.value.push({agent: 'me',is_loading: true})
+    const user_text=await transcribeText(base64)
+    conversations.value.pop()
+    conversations.value.push({agent: 'me',message: user_text,is_loading: false})
+  }catch(e)
+  {
+    conversations.value.pop()
+    console.error(e)
+
+  }
+
 
 }
 
+async function transcribeText(base64: string)
+{
 
+  const file=base64ToFile(base64,'audio.wav')
+  const response=await chatgpt().audio.transcriptions.create({model: 'whisper-1',file})
+  return response.text
+
+
+
+
+}
+
+function base64ToFile(base64_file : string, fileName : string) {
+
+
+  const mime_type='audio/wav'
+
+
+  const byteString = atob(base64_file);
+
+
+  const byteArray = new Uint8Array(byteString.length);
+  for (let i = 0; i < byteString.length; i++) {
+    byteArray[i] = byteString.charCodeAt(i);
+  }
+
+
+  return new File([byteArray], fileName, { type: mime_type });
+}
 
 
 </script>
